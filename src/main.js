d864bdffb33378279d7abe00a7a743f4b2450c45
Pinoles <danieldmjr@gmail.com>
2021-07-28 20:15:18 -0500

    tercer

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+// Agregamos la URL base de nuestra API
+axios.defaults.baseURL = 'http://localhost:4000/api';
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(VueAxios, axios).mount('#app')
